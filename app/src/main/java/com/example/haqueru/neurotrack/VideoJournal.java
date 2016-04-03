7a329312e23c57c0c3ba7d9737a6cc03e296b018
haqueru <rafihaque90@gmail.com>
2016-04-03 18:20:39 -0500

    Commiting Base Code for First Time

--- app/src/main/java/com/example/haqueru/neurotrack/VideoJournal.java
+++ app/src/main/java/com/example/haqueru/neurotrack/VideoJournal.java
@@ -3,11 +3,8 @@ package com.example.haqueru.neurotrack;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -49,14 +46,7 @@ public class VideoJournal extends AppCompatActivity implements OnDateSelectedLis
         //Setup initial text
         textView.setText(getSelectedDatesString());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
 
 
