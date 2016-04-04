cd3b4afd2c70f9ed76d89aaaeb6f097f14a2a4a4
haqueru <rafihaque90@gmail.com>
2016-04-04 15:33:44 -0400

    Commiting Base Code for First Time

--- app/src/main/java/com/example/haqueru/neurotrack/VideoJournal.java
+++ app/src/main/java/com/example/haqueru/neurotrack/VideoJournal.java
@@ -1,10 +1,20 @@
 package com.example.haqueru.neurotrack;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
@@ -27,8 +37,29 @@ public class VideoJournal extends AppCompatActivity implements OnDateSelectedLis
     @Bind(R.id.calendarView)
     MaterialCalendarView widget;
 
-    @Bind(R.id.textView)
-    TextView textView;
+    //@Bind(R.id.textView)
+    //TextView textView;
+
+    @Bind(R.id.videoList)
+    ListView list;
+
+
+    String[] itemname ={
+            "Video 1",
+            "Video 2",
+            "Video 3",
+            "Video 4"
+    };
+    Integer[] imgid={
+            R.drawable.video,
+            R.drawable.video,
+            R.drawable.video,
+            R.drawable.video
+    };
+
+    String[] itemdescription={
+            ""
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,24 +75,47 @@ public class VideoJournal extends AppCompatActivity implements OnDateSelectedLis
         setSupportActionBar(toolbar);
 
         //Setup initial text
-        textView.setText(getSelectedDatesString());
+        //textView.setText(getSelectedDatesString());
 
-
-
-
-
-
-
-
-
+        // take video on floating action press
+        takeVideo();
 
 
 
     }
 
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    private void dispatchTakeVideoIntent() {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+
+    private void takeVideo(){
+        // floating action button
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakeVideoIntent();
+            }
+        });
+
+    }
+
+
+
+
+
     @Override
     public void onDateSelected(@NonNull MaterialCalendarView widget, @Nullable CalendarDay date, boolean selected) {
-        textView.setText(getSelectedDatesString());
+        // create list of games by binding custom adapter to list
+        CustomListAdapter adapter = new CustomListAdapter(this, itemname, imgid);
+
+        list.setAdapter(adapter);
+        //textView.setText(getSelectedDatesString());
     }
 
     @Override
@@ -76,6 +130,41 @@ public class VideoJournal extends AppCompatActivity implements OnDateSelectedLis
             return "No Selection";
         }
         return FORMATTER.format(date.getDate());
+    }
+
+
+    public class CustomListAdapter extends ArrayAdapter<String> {
+
+        private final Activity context;
+        private final String[] itemname;
+        private final Integer[] imgid;
+
+        public CustomListAdapter(Activity context, String[] itemname, Integer[] imgid) {
+            super(context, R.layout.mylist, itemname);
+            // TODO Auto-generated constructor stub
+
+            this.context=context;
+            this.itemname=itemname;
+            this.imgid=imgid;
+        }
+
+        public View getView(int position,View view,ViewGroup parent) {
+            LayoutInflater inflater=context.getLayoutInflater();
+            View rowView=inflater.inflate(R.layout.mylist, null,true);
+
+            TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
+            //ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            //TextView extratxt = (TextView) rowView.findViewById(R.id.textView1);
+
+            txtTitle.setText(itemname[position]);
+            txtTitle.setTextColor(Color.parseColor("#000000"));
+            //txtTitle.setHeight(10);
+            //imageView.setImageResource(imgid[position]);
+            //extratxt.setText("Description "+itemname[position]);
+
+            return rowView;
+
+        }
     }
 
 
